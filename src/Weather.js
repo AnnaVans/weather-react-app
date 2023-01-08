53f00ebf830a8b16c7074bdf5de07793f6d9441f
AnnaVans <119419794+AnnaVans@users.noreply.github.com>
2023-01-08 13:15:50 +0100

    functions updated

--- src/Weather.js
+++ src/Weather.js
@@ -4,16 +4,18 @@ import "./App.css";
 import axios from "axios";
 
 export default function Weather() {
-  let weatherData = {
-    city: "Bratislava",
-    temperature: "4",
-    time: "05:16",
-    description: "Foggy",
-    feelsLike: "1",
-    wind: "5",
-    humidity: "81",
-  };
+  const [loading, setLoading] = useState(false);
+  const [city, changeCity] = useState("");
 
+  function updateCity(event) {
+    changeCity(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=4f6484d9a7c6987f3133d7c6fd4bd3b8`;
+    axios.get(url).then(showTemp);
+  }
   function showTemp(response) {
     setLoading(true);
     let icon = (
@@ -22,55 +24,50 @@ export default function Weather() {
         src={`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`}
       />
     );
-    showTemperature(Math.round(response.data.main.temp));
-    showWind(Math.round(response.data.wind.speed));
-    showHumidity(response.data.main.humidity);
-    showDescription(response.data.weather[0].main);
-    showIcon(icon);
-  }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=4f6484d9a7c6987f3133d7c6fd4bd3b8`;
-    axios.get(url).then(showTemp);
-  }
-
-  return (
-    <div className="Weather">
-      <h1>{weatherData.city}</h1>
-      <h2>
-        <span className="actualTemp">{weatherData.temperature}</span>
-        <span className="celsius">°C</span>
-      </h2>
-      <h3>
-        <span class="time"> {weatherData.time} </span>|
-        <span class="description"> {weatherData.description}</span>
+    return (
+      <div className="Weather">
+        <h1>{city}</h1>
+        <h2>
+          <span className="actualTemp">
+            {Math.round(response.data.main.temp)}
+          </span>
+          <span className="celsius">°C</span>
+        </h2>
+        <h3>
+          <span class="time"> </span>|
+          <span class="description"> {response.data.weather[0].main}</span>
+          <div>
+            Feels-like temperature °C | Wind speed{" "}
+            {Math.round(response.data.wind.speed)}m/s | Humidity{" "}
+            {response.data.main.humidity} %
+          </div>
+        </h3>
         <div>
-          Feels-like temperature {weatherData.feelsLike}°C | Wind speed{" "}
-          {weatherData.wind}m/s | Humidity {weatherData.humidity} %
-        </div>
-      </h3>
-      <div>
-        <div className="Weather">
-          <div className="container">
-            <div className="row">
-              <div className="offset-sm-1 col-sm-10">
-                <form onSubmit={handleSubmit}>
-                  <div class="mb-3 location">
-                    <label for="exampleInputEmail1" class="form-label"></label>
-                    <input
-                      type="text"
-                      placeholder="Enter location"
-                      class="form-control"
-                      autoComplete="off"
-                    />
-                  </div>
-                </form>
+          <div className="Weather">
+            <div className="container">
+              <div className="row">
+                <div className="offset-sm-1 col-sm-10">
+                  <form onSubmit={handleSubmit}>
+                    <div class="mb-3 location">
+                      <label
+                        for="exampleInputEmail1"
+                        class="form-label"
+                      ></label>
+                      <input
+                        type="text"
+                        placeholder="Enter location"
+                        class="form-control"
+                        autoComplete="off"
+                      />
+                    </div>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
